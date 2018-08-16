dacaae2550cdc3d1715fe26d521b42145fe4f35f
Rudy Ondolan <rudy.ondolan@myrepublic.net>
2018-08-16 16:44:00 +0700

    move options to json file

--- app/components/Form/index.js
+++ app/components/Form/index.js
@@ -4,41 +4,7 @@ import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
 import messages from './messages';
-
-const countryOptions = [
-  { key: 'AUD', value: 'AUD', text: 'AUD', flag: 'au' },
-  { key: 'BGN', value: 'BGN', text: 'BGN', flag: 'bg' },
-  { key: 'BRL', value: 'BRL', text: 'BRL', flag: 'br' },
-  { key: 'CAD', value: 'CAD', text: 'CAD', flag: 'ca' },
-  { key: 'CHF', value: 'CHF', text: 'CHF', flag: 'ch' },
-  { key: 'CNY', value: 'CNY', text: 'CNY', flag: 'cn' },
-  { key: 'CZK', value: 'CZK', text: 'CZK', flag: 'cz' },
-  { key: 'DKK', value: 'DKK', text: 'DKK', flag: 'dk' },
-  { key: 'GBP', value: 'GBP', text: 'GBP', flag: 'gb' },
-  { key: 'HKD', value: 'HKD', text: 'HKD', flag: 'hk' },
-  { key: 'HRK', value: 'HRK', text: 'HRK', flag: 'hr' },
-  { key: 'HUF', value: 'HUF', text: 'HUF', flag: 'hu' },
-  { key: 'IDR', value: 'IDR', text: 'IDR', flag: 'id' },
-  { key: 'ILS', value: 'ILS', text: 'ILS', flag: 'il' },
-  { key: 'INR', value: 'INR', text: 'INR', flag: 'in' },
-  { key: 'ISK', value: 'ISK', text: 'ISK', flag: 'is' },
-  { key: 'JPY', value: 'JPY', text: 'JPY', flag: 'jp' },
-  { key: 'KRW', value: 'KRW', text: 'KRW', flag: 'kr' },
-  { key: 'MXN', value: 'MXN', text: 'MXN', flag: 'mx' },
-  { key: 'MYR', value: 'MYR', text: 'MYR', flag: 'my' },
-  { key: 'NOK', value: 'NOK', text: 'NOK', flag: 'no' },
-  { key: 'NZD', value: 'NZD', text: 'NZD', flag: 'nz' },
-  { key: 'PHP', value: 'PHP', text: 'PHP', flag: 'ph' },
-  { key: 'PLN', value: 'PLN', text: 'PLN', flag: 'pl' },
-  { key: 'RON', value: 'RON', text: 'RON', flag: 'ro' },
-  { key: 'RUB', value: 'RUB', text: 'RUB', flag: 'ru' },
-  { key: 'SEK', value: 'SEK', text: 'SEK', flag: 'se' },
-  { key: 'SGD', value: 'SGD', text: 'SGD', flag: 'sg' },
-  { key: 'THB', value: 'THB', text: 'THB', flag: 'th' },
-  { key: 'TRY', value: 'TRY', text: 'TRY', flag: 'tr' },
-  { key: 'USD', value: 'USD', text: 'USD', flag: 'us' },
-  { key: 'ZAR', value: 'ZAR', text: 'ZAR', flag: 'za' },
-];
+import options from './data.json';
 
 function Form({ onChange, onClick }) {
   return (
@@ -51,7 +17,7 @@ function Form({ onChange, onClick }) {
               fluid
               search
               selection
-              options={countryOptions}
+              options={options}
               onChange={(e, data) => onChange(data.value)}
             />
           </Grid.Column>
